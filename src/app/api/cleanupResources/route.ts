b51d0509601406ac452c893e61720e063a10848c
Ekansh Jain <ejekanshjain@gmail.com>
2023-12-27 11:13:59 +0530

    move cleanup resources to lib

--- src/app/api/cleanupResources/route.ts
+++ src/app/api/cleanupResources/route.ts
@@ -1,31 +1,7 @@
+import { cleanupResources } from '@/lib/cleanupResources'
 import { NextResponse } from 'next/server'
 
-import { prisma } from '@/lib/db'
-import { storageClient } from '@/lib/storageClient'
-
 export async function POST() {
-  const attachments = await prisma.resource.findMany({
-    where: {
-      qrCodeId: null
-    }
-  })
-  const toDelete: string[] = []
-  await Promise.all(
-    attachments.map(async a => {
-      try {
-        await storageClient.deleteFile(a.newFilename)
-        toDelete.push(a.id)
-      } catch (err) {}
-    })
-  )
-  if (toDelete.length)
-    await prisma.resource.deleteMany({
-      where: {
-        id: {
-          in: toDelete
-        }
-      }
-    })
-
+  await cleanupResources()
   return NextResponse.json({ success: true })
 }
