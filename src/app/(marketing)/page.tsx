91c2818f586d4c78ba1bcd82b22fea72c9f67c04
Ekansh Jain <ejekanshjain@gmail.com>
2024-03-09 16:30:09 +0530

    update landing page, pricing cards

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { BarGraph } from '@/components/graphs/bar-graph'
 import { PricingCards } from '@/components/pricing-cards'
 import { QRPlayground } from '@/components/qr-playground'
+import { BackgroundBeams } from '@/components/ui/background-beams'
 import { Button } from '@/components/ui/button'
 import { getAuthSession } from '@/lib/auth'
 
@@ -41,7 +42,31 @@ const Home = async () => {
   const session = await getAuthSession()
 
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="relative flex min-h-screen flex-col">
+      <section className="relative w-full py-12 md:py-24 lg:py-32">
+        <div className="container relative z-10 mx-auto px-4 md:px-6">
+          <div className="flex flex-col items-center justify-center space-y-4 text-center">
+            <div className="space-y-2">
+              <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+                Generate Your QR Code
+              </h1>
+              <p className="mx-auto max-w-[700px] text-gray-500 dark:text-gray-400 md:text-xl">
+                Enter your desired content and generate a QR code instantly.
+                Customize the logo to your needs.
+              </p>
+            </div>
+            <div className="w-full max-w-md pt-5">
+              <Link href={session?.user ? '/qr-codes/new' : '/login'}>
+                <Button size="lg">Generate QR Code</Button>
+              </Link>
+            </div>
+            <div className="max-w-2xl flex-col space-y-2 pt-8">
+              <QRPlayground />
+            </div>
+          </div>
+        </div>
+        <BackgroundBeams />
+      </section>
       <section className="w-full py-12 md:py-24 lg:py-32">
         <div className="container mx-auto px-4 md:px-6">
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
--- src/components/pricing-cards.tsx
+++ src/components/pricing-cards.tsx
@@ -11,53 +11,65 @@ import { Separator } from '@/components/ui/separator'
 import { getAuthSession } from '@/lib/auth'
 import { plans } from '@/lib/subscription'
 import { PricingButton } from './pricing-button'
+import { BackgroundGradient } from './ui/background-gradient'
 
 export const PricingCards = async () => {
   const session = await getAuthSession()
 
   return (
     <div className="grid w-full gap-6 lg:grid-cols-3 lg:gap-12">
-      {plans.map(plan => (
-        <Card
-          key={plan.name}
-          className={`flex flex-col justify-between ${plan.promoted ? '' : ''}`}
-        >
-          <div>
-            <CardHeader className="flex flex-col gap-4">
-              <CardTitle>{plan.name}</CardTitle>
-              <CardDescription className="text-gray-500">
-                {plan.description}
-              </CardDescription>
-              <div className="flex items-end">
-                <h4 className="text-3xl font-semibold">${plan.price}</h4>
-                {plan.per ? (
-                  <span className="ml-2 text-sm font-light text-gray-600">
-                    / {plan.per}
-                  </span>
-                ) : null}
-              </div>
-            </CardHeader>
-            <Separator />
-            <CardContent className="py-6">
-              <ul className="space-y-2 text-left text-sm">
-                {plan.features.map(feature => (
-                  <li key={feature}>
-                    <Icons.check className="mr-2 inline-block h-4 w-4" />
-                    {feature}
-                  </li>
-                ))}
-              </ul>
-            </CardContent>
-          </div>
-          <CardFooter>
-            <PricingButton
-              session={!!session?.user}
-              planStripePriceId={plan.stripePriceId}
-              userStripePriceId={session?.user.stripePriceId}
-            />
-          </CardFooter>
-        </Card>
-      ))}
+      {plans.map(plan => {
+        const Inner = (
+          <>
+            <div className="flex flex-col justify-between">
+              <CardHeader className="flex flex-col gap-4">
+                <CardTitle>{plan.name}</CardTitle>
+                <CardDescription className="text-gray-500">
+                  {plan.description}
+                </CardDescription>
+                <div className="flex items-end">
+                  <h4 className="text-3xl font-semibold">${plan.price}</h4>
+                  {plan.per ? (
+                    <span className="ml-2 text-sm font-light text-gray-600">
+                      / {plan.per}
+                    </span>
+                  ) : null}
+                </div>
+              </CardHeader>
+              <Separator />
+              <CardContent className="py-6">
+                <ul className="space-y-2 text-left text-sm">
+                  {plan.features.map(feature => (
+                    <li key={feature}>
+                      <Icons.check className="mr-2 inline-block h-4 w-4" />
+                      {feature}
+                    </li>
+                  ))}
+                </ul>
+              </CardContent>
+            </div>
+            <CardFooter>
+              <PricingButton
+                session={!!session?.user}
+                planStripePriceId={plan.stripePriceId}
+                userStripePriceId={session?.user.stripePriceId}
+              />
+            </CardFooter>
+          </>
+        )
+        return plan.promoted ? (
+          <BackgroundGradient
+            className="flex h-full flex-col justify-between rounded-[22px] bg-background p-4 sm:p-8"
+            key={plan.name}
+          >
+            {Inner}
+          </BackgroundGradient>
+        ) : (
+          <Card className="flex h-full flex-col justify-between rounded-[22px] bg-background p-4 sm:p-8">
+            {Inner}
+          </Card>
+        )
+      })}
     </div>
   )
 }
