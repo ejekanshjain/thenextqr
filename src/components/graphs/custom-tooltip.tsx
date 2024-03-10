1673e0fd4d792635df344ad07d153518ffd6437e
Ekansh Jain <ejekanshjain@gmail.com>
2024-03-10 22:38:43 +0530

    fix icon and custom tooltip

--- src/components/graphs/custom-tooltip.tsx
+++ src/components/graphs/custom-tooltip.tsx
@@ -4,8 +4,8 @@ export const CustomTooltip: FC<any> = ({ active, payload }) => {
   if (active && payload && payload.length) {
     return (
       <div>
-        <p>{payload[0].payload.date}</p>
-        <p>Count: {payload[0].value}</p>
+        <p>{payload[0].payload.name}</p>
+        <p>Count: {payload[0].payload.count}</p>
       </div>
     )
   }
