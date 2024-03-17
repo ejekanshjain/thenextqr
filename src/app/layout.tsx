36b1b65656ee64ba4cf8d69c6326e58d58296677
Ekansh Jain <ejekanshjain@gmail.com>
2024-03-17 16:47:47 +0530

    change font, to geist font, added vercel things back in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 // import { Analytics } from '@vercel/analytics/react'
 // import { SpeedInsights } from '@vercel/speed-insights/next'
+import { GeistMono } from 'geist/font/mono'
+import { GeistSans } from 'geist/font/sans'
 import { Metadata } from 'next'
 import { ThemeProvider } from 'next-themes'
-import { Inter as FontSans } from 'next/font/google'
 import { ReactNode } from 'react'
 
 import { Toaster } from '@/components/ui/toaster'
@@ -11,11 +12,6 @@ import { env } from '@/env.mjs'
 import { cn } from '@/lib/cn'
 import { siteConfig } from '@/lib/siteConfig'
 import '@/styles/globals.css'
-
-const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans'
-})
 
 export const metadata: Metadata = {
   title: siteConfig.name,
@@ -28,7 +24,8 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
       <body
         className={cn(
           'min-h-screen bg-background font-sans antialiased',
-          fontSans.variable
+          GeistSans.variable,
+          GeistMono.variable
         )}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,8 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-sans)', ...fontFamily.sans]
+        sans: ['var(--font-geist-sans)', ...fontFamily.sans],
+        mono: ['var(--font-geist-mono)', ...fontFamily.mono]
       },
       colors: {
         border: 'hsl(var(--border))',
